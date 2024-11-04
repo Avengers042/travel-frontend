b0886f86bfd930dfe026549c378c5287a4222eb1
lyanna.cristina <lyanna.cristina@fornecedores.sicoob.com.br>
2024-11-04 09:44:23 -0300

    feat(config): add de rotas, add css global, add index home, add config biome

--- projeto-viagens/src/app/app.component.ts
+++ projeto-viagens/src/app/app.component.ts
@@ -1,13 +1,14 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from "@angular/core";
+// biome-ignore lint/style/useImportType: <explanation>
+import { RouterOutlet } from "@angular/router";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+	selector: "app-root",
+	standalone: true,
+	imports: [RouterOutlet],
+	templateUrl: "./app.component.html",
+	styleUrl: "./app.component.css",
 })
 export class AppComponent {
-  title = 'projeto-viagens';
+	title = "projeto-viagens";
 }
--- projeto-viagens/src/app/app.routes.ts
+++ projeto-viagens/src/app/app.routes.ts
@@ -1,3 +1,10 @@
-import { Routes } from '@angular/router';
+import type { Routes } from "@angular/router";
+import { FormularioComponent } from "./funcionalidades/formulario/componente/formulario/formulario.component";
+import { HomeComponent } from "./funcionalidades/home/componente/home.component";
+import { ResultadoComponent } from "./funcionalidades/resultado/componente/resultado.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+	{ path: "", component: HomeComponent },
+	{ path: "formulario", component: FormularioComponent },
+	{ path: "resultado", component: ResultadoComponent },
+];
